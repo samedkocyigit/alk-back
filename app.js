582a3d5d88d9e0cc1543e46f04623b0bb920b3a3
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-01 17:45:25 +0300

    app.js file has been created and some middlewares defined

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const viewRouter = require("./src/routes/viewRoutes");
+const userRouter = require("./src/routes/userRoutes");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+// Routes
+app.use("/", viewRouter);
+app.use("/users", userRouter);
+
+// app.use("/urun", productRouter);
+// app.use("/kargo-takibi", cargoTrackingRouter);
+// app.use("/hakkimizda", aboutUsRouter);
+// app.use("/iletisim", contactRouter);
+// app.use("/iletisim-formu", contactFormRouter);
+// app.use("/fiyat-listesi", priceListRouter);
+// app.use("/blog", blogRouter);
+// app.use("/videolar", videosRouter);
+// app.use("/kategori", categoryRouter);
+
+//app.use(globalErrorHandler);
+
+module.exports = app;
