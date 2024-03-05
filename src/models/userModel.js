66358b617a39218acdd5a8d9a1a87fce372ee298
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-05 16:52:41 +0300

    Small Updates and Refactoring

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -72,10 +72,12 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
+
+  next();
 });
 
 userSchema.pre("save", function (next) {
-  if (!this.isModified("password") || this.name) return next();
+  if (!this.isModified("password") || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
   next();
