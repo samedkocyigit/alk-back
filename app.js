8d5733027cb64426312d11a9bcc6cdd84b6de476
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-08 15:46:28 +0300

    comment route has been created

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-const viewRouter = require("./src/routes/viewRoutes");
 const userRouter = require("./src/routes/userRoutes");
 const productRouter = require("./src/routes/productRoutes");
 const categoryRouter = require("./src/routes/categoryRoutes");
+const commentRouter = require("./src/routes/commentRoutes");
 
 const app = express();
 
@@ -18,20 +18,10 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use("/", viewRouter);
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/category", categoryRouter);
-
-// app.use("/urun", productRouter);
-// app.use("/kargo-takibi", cargoTrackingRouter);
-// app.use("/hakkimizda", aboutUsRouter);
-// app.use("/iletisim", contactRouter);
-// app.use("/iletisim-formu", contactFormRouter);
-// app.use("/fiyat-listesi", priceListRouter);
-// app.use("/blog", blogRouter);
-// app.use("/videolar", videosRouter);
-// app.use("/kategori", categoryRouter);
+app.use("/comments", commentRouter);
 
 //app.use(globalErrorHandler);
 
