e968d6e9354ef78f7bf75fdfa7eb53b4e884ec07
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-08 15:48:02 +0300

    categories fields has been removed and added some new fields

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,52 +1,61 @@
 const mongoose = require("mongoose");
 const { default: slugify } = require("slugify");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Product must hava a name"],
-  },
-  slug: String,
-  brand: {
-    type: String,
-    required: [true, "Product must have a brand"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Product must have a price"],
-  },
-  stock_code: {
-    type: Number,
-    required: [true, "Product must have a stock code"],
-  },
-  summary: {
-    type: String,
-    required: [true, "Product must have a summary"],
-  },
-  manufactorer_code: {
-    type: String,
-    required: [true, "Product must have a brand"],
-  },
-  photo: String,
-  categories: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: "Category",
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product must hava a name"],
     },
-  ],
+    slug: String,
+    brand: {
+      type: String,
+      required: [true, "Product must have a brand"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Product must have a price"],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 2,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+      set: (val) => Math.round(val * 10) / 10, //4.66666, 46.66666, 4.7,47
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    stock_code: {
+      type: Number,
+      required: [true, "Product must have a stock code"],
+    },
+    summary: {
+      type: String,
+      required: [true, "Product must have a summary"],
+    },
+    manufactorer_code: {
+      type: String,
+      required: [true, "Product must have a brand"],
+    },
+    photo: String,
+    category_name: String,
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+productSchema.virtual("comment", {
+  ref: "Comment",
+  foreignField: "user",
+  localField: "_id",
 });
 
 productSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
-
-  next();
-});
-
-productSchema.pre(/^find/, function (next) {
-  this.populate({
-    path: "categories.sub_category",
-    select: "-__v ",
-  });
 
   next();
 });
