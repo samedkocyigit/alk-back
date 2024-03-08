57c12a8a1d0e208aa441fcda2e1a4cadaa013e5d
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-08 15:47:00 +0300

    comment model has been created and also defined

---
+++ src/models/commentModel.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose");
+const Product = require("../models/productModel");
+
+const commentSchema = new mongoose.Schema(
+  {
+    title: { type: String, require: [true, "Title can not be empty!"] },
+    comment: { type: String, require: [true, "Comment can not be empty!"] },
+    rating: { type: Number, min: 1, max: 5 },
+    createdAt: { type: Date, default: Date.now, select: false },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: [true, "Comment must belong to a product"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Comment must belong to a user"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+commentSchema.index({ product: 1, user: 1 }, { unique: true });
+
+commentSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "name surname",
+  });
+  next();
+});
+
+// commentSchema.statics.calcAverageRatings = async function (productId) {
+//   const stats = await this.aggregate([
+//     {
+//       $match: { product: productId },
+//     },
+//     {
+//       $group: {
+//         _id: "$product",
+//         nRating: { $sum: 1 },
+//         avgRating: { $avg: "$rating" },
+//       },
+//     },
+//   ]);
+//   // console.log(stats);
+
+//   if (stats.length > 0) {
+//     await Product.findByIdAndUpdate(productId, {
+//       ratingsQuantity: stats[0].nRating,
+//       ratingsAverage: stats[0].avgRating,
+//     });
+//   } else {
+//     await Product.findByIdAndUpdate(productId, {
+//       ratingsQuantity: 0,
+//       ratingsAverage: 4.5,
+//     });
+//   }
+// };
+
+// commentSchema.post("save", function () {
+//   // this points to current review
+//   this.constructor.calcAverageRatings(this.product);
+// });
+
+// // findByIdAndUpdate
+// // findByIdAndDelete
+// commentSchema.pre(/^findOneAnd/, async function (next) {
+//   this.r = await this.findOne();
+//   // console.log(this.r);
+//   next();
+// });
+
+// commentSchema.post(/^findOneAnd/, async function () {
+//   // await this.findOne(); does NOT work here, query has already executed
+//   await this.r.constructor.calcAverageRatings(this.r.product);
+// });
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
