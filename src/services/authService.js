552aa93dd2064b56291d7749248638310b4097e2
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-04-02 10:20:56 +0300

    trying to fix the jwt malformed mistake

--- src/services/authService.js
+++ src/services/authService.js
@@ -54,27 +54,29 @@ exports.signUp = catchAsync(async (req, res, next) => {
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
-  // check if email and password exist
-  if (!email || !password) {
-    return next(new AppError("Please provide email and password", 401));
+  try {
+    // check if email and password exist
+    if (!email || !password) {
+      return next(new AppError("Please provide email and password", 401));
+    }
+
+    // Check if user exist and password is correct
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError("Incorrect email or password", 401));
+    }
+
+    // If everything ok, send token client
+    const token = signToken(user._id);
+
+    res.status(200).json({
+      status: "success",
+      token,
+    });
+  } catch (error) {
+    return next(error);
   }
-
-  // Check if user exist and password is correct
-  const user = await User.findOne({ email }).select("+password");
-  const correct = await user.correctPassword(password, user.password);
-
-  if (!user || !correct)
-    return next(new AppError("Incorrect email or password", 401));
-
-  // If everything ok, send token client
-  createSendToken(user, 201, res);
-
-  const token = signToken(user._id);
-
-  res.status(200).json({
-    status: "success",
-    token,
-  });
 });
 
 exports.logout = (req, res) => {
@@ -87,9 +89,9 @@ exports.logout = (req, res) => {
 };
 
 exports.protect = catchAsync(async (req, res, next) => {
-  // Getting token and check if its there
+  // 1) Getting token and check of its there
   let token;
-  //console.log(req.headers.authorization);
+  console.log(req.headers.authorization);
 
   if (
     req.headers.authorization &&
@@ -100,24 +102,34 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.cookies.jwt;
   }
 
+  console.log(token, "messi");
+  // console.log(token)
   if (!token) {
     return next(
       new AppError("You are not logged in! Please log in to get access", 401)
     );
   }
 
-  // Verify token
+  // 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
-  // Check is user still exist
+
+  // 3) Check if user still exist
   const currentUser = await User.findById(decoded.id);
   if (!currentUser) {
     return next(
-      new AppError("The user belonging to this token does not exist", 401)
+      new AppError(
+        "The user belonging to this token does no longer exist.",
+        401
+      )
     );
   }
 
-  // Check if user changed password after the token was issued
+  // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed password! Please log in again.", 401)
+    );
+  }
 
   // Grant access to protected route
   req.user = currentUser;
