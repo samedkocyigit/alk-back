4989e831c20f4a94d91787294e26a2e149a2ba1a
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-04-16 09:11:40 +0300

    subcategory name implementation has been added at addProductAtSubCategory

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -128,7 +128,9 @@ exports.addProductAtSubCategory = catchAsync(async (req, res, next) => {
     return next(new AppError("No product found with given ID.", 404));
   }
 
-  product.category_name = subCategory.sub_category_name;
+  console.log("flan filan =>", category);
+  product.category_name = category.category_name;
+  product.subCategory_name = subCategory.sub_category_name;
   subCategory.sub_product.push(productId);
 
   await product.save();
