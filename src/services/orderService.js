5bd7284c3c8589f93d03eaf60dca67681866bd2f
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-05-30 08:50:08 +0300

    orderService func has been created

---
+++ src/services/orderService.js
@@ -0,0 +1,77 @@
+const Order = require("../models/orderModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllOrders = catchAsync(async (req, res, next) => {
+  const orders = await Order.find();
+
+  if (!orders) {
+    return new AppError("Find no document in Order model");
+  }
+
+  res.status(200).json({
+    status: "success",
+    requiredAt: orders.length,
+    data: {
+      data: orders,
+    },
+  });
+});
+
+exports.getOrder = catchAsync(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return new AppError("No document with that id", 400);
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { data: order },
+  });
+});
+
+exports.createOrder = catchAsync(async (req, res, next) => {
+  const newOrder = await Order.create(req.body);
+
+  if (!newOrder) {
+    return new AppError("Creation failed", 400);
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: newOrder,
+    },
+  });
+});
+
+exports.updateOrder = catchAsync(async (req, res, next) => {
+  const updatedOrder = await Order.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!updatedOrder) {
+    return new AppError("fail updated", 400);
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: updatedOrder,
+    },
+  });
+});
+
+exports.deleteOrder = catchAsync(async (req, res, next) => {
+  const deletedOrder = await Order.findByIdAndUpdate(req.params.id);
+
+  if (!deletedOrder) {
+    return new AppError("Deletion failed", 400);
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: null,
+  });
+});
